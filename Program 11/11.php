b3d342560069b1b0a899b66d885ac5272dddd922
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-18 13:55:36 +0530

    Fixed indentation

--- Program 11/11.php
+++ Program 11/11.php
@@ -1,13 +1,15 @@
 <?php
 
 $expiry = 24*60*60+time();
+
 if (isset($_COOKIE["visited"])){
 	$value=$_COOKIE["visited"]; 
 	print "Last Visited on: $value"; 
-	}
+}
+
 else{
-setcookie("visited",date("G:i:s a - m/d/y"),"$expiry");
-print "NEW COOKIE SET !";
+	setcookie("visited",date("G:i:s a - m/d/y"),"$expiry");
+	print "NEW COOKIE SET !";
 }
 
 ?>
