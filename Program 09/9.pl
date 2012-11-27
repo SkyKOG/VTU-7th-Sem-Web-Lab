b9b2822cdbd43fb1308f00e6fe1f361cdfdb88be
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-27 17:24:14 +0530

    Update

--- Program 09/9.pl
+++ Program 09/9.pl
@@ -3,7 +3,6 @@
 
 print "Content-type:text/html\n\n";
 ($s,$m,$h) = localtime(time);
-print "<META HTTP-EQUIV=refresh content=0 ";
-print "URL=http://localhost/cgi-bin/9.pl>";
+print "<META HTTP-EQUIV=refresh content=0 URL=http://localhost/cgi-bin/9.pl>";
 
 print "<center>Current time is:<br /> $h:$m:$s </center>";
