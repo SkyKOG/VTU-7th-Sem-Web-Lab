f7c803a0bf331f47fc396542ea76d2c4d9a6e357
Akash Devaraju <akashdevaraju@gmail.com>
2012-11-28 22:40:52 +0530

    Optimised

--- Program 08/8a.pl
+++ Program 08/8a.pl
@@ -3,10 +3,10 @@
 
 print "Content-type:text/html\n\n";
 use CGI;
+
 $a = new CGI;
 $name = $a->param("name");
-$num=4;
-$rand_num=rand($num);
+$rand_num=rand(4);
 @greet=('hello','hi','Salut','Bonjeur');
-print "$greet[$rand_num]";
-print " $name";
+
+print "$greet[$rand_num] $name";
